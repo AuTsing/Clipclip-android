5b4a7ecc72e8de27b11f9b3077521320196c3914
AuTsing <763025696@qq.com>
2026-04-22 13:07:46 +0800

    0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.at.clipclip"
         minSdk = 26
         targetSdk = 36
-        versionCode = 100
-        versionName = "0.1.0"
+        versionCode = 101
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
